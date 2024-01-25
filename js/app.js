181fc0b8aa09fd21ee30d19a0e26f925eac28d20
Juanchi <toranzos628@gmail.com>
2024-01-25 14:59:33 -0300

    comit para que gaby pueda ver los avances

---
+++ js/app.js
@@ -0,0 +1,162 @@
+
+// card 1
+let contenidoExpandido = false;
+
+function verMas() {
+    const seccionServicio = document.getElementById('seccionServicio');
+    const btnVerMas = document.getElementById('btnVerMas');
+    const parrafoServicio = document.getElementById('parrafoServicio');
+
+    if (!contenidoExpandido) {
+        // Mostrar más contenido
+        parrafoServicio.innerHTML += ` <br> <p class="text-justify p-2"> Ofrecemos servicios integrales de construcción para proyectos civiles, desde la planificación hasta la entrega final. Nos especializamos en estructuras sólidas, utilizando tecnologías avanzadas y materiales de alta calidad. Contamos con un experimentado equipo comprometido con la excelencia y altos estándares de calidad y seguridad. Además de la construcción, brindamos servicios de gestión de proyectos eficientes. Colaboramos estrechamente con nuestros clientes para crear espacios funcionales que reflejen su visión y estilo. Al elegirnos, obtienes confiabilidad y compromiso para convertir tus ideas en soluciones de construcción sólidas y sostenibles. </p>`;
+        btnVerMas.innerHTML = 'Ocultar';
+        btnVerMas.className = 'btn btn-danger';
+    } else {
+        // Restaurar el contenido original
+        parrafoServicio.innerHTML = 'Servicio de construcción para proyectos civiles.';
+        btnVerMas.innerHTML = 'Ver mas';
+        btnVerMas.className = 'btn btn-primary';
+    }
+
+    contenidoExpandido = !contenidoExpandido;
+}
+ //card 2
+ let contenidoExpandido1 = false;
+
+function verMas1() {
+    const seccionServicio1 = document.getElementById('seccionServicio1');
+    const btnVerMas1 = document.getElementById('btnVerMas1');
+    const parrafoServicio1 = document.getElementById('parrafoServicio1');
+
+    if (!contenidoExpandido1) {
+        // Mostrar más contenido
+        parrafoServicio1.innerHTML += ` <br> <p class="text-justify p-2"> El Mantenimiento de Maquinarias Industriales asegura el rendimiento óptimo de equipos en entornos industriales. Incluye inspecciones, ajustes y reparaciones para prevenir fallas, maximizando la eficiencia y minimizando tiempos de inactividad. Profesionales especializados garantizan un funcionamiento seguro y eficaz, contribuyendo a la continuidad operativa. </p>`;
+        btnVerMas1.innerHTML = 'Ocultar';
+        btnVerMas1.className = 'btn btn-danger';
+    } else {
+        // Restaurar el contenido original
+        parrafoServicio1.innerHTML = 'Servicio de mantenimiento y reparación de maquinarias.';
+        btnVerMas1.innerHTML = 'Ver mas';
+        btnVerMas1.className = 'btn btn-primary';
+    }
+
+    contenidoExpandido1 = !contenidoExpandido1;
+}
+//card 3
+let contenidoExpandido2 = false;
+
+function verMas2() {
+    const seccionServicio2 = document.getElementById('seccionServicio2');
+    const btnVerMas2 = document.getElementById('btnVerMas2');
+    const parrafoServicio2 = document.getElementById('parrafoServicio2');
+
+    if (!contenidoExpandido2) {
+        // Mostrar más contenido
+        parrafoServicio2.innerHTML += ` <br> <p class="text-justify p-2"> Ofrecemos servicios integrales de construcción para proyectos civiles, desde la planificación hasta la entrega final. Nos especializamos en estructuras sólidas, utilizando tecnologías avanzadas y materiales de alta calidad. Contamos con un experimentado equipo comprometido con la excelencia y altos estándares de calidad y seguridad. Además de la construcción, brindamos servicios de gestión de proyectos eficientes. Colaboramos estrechamente con nuestros clientes para crear espacios funcionales que reflejen su visión y estilo. Al elegirnos, obtienes confiabilidad y compromiso para convertir tus ideas en soluciones de construcción sólidas y sostenibles. </p>`;
+        btnVerMas2.innerHTML = 'Ocultar';
+        btnVerMas2.className = 'btn btn-danger';
+    } else {
+        // Restaurar el contenido original
+        parrafoServicio2.innerHTML = 'Servicio de construcción para proyectos civiles.';
+        btnVerMas2.innerHTML = 'Ver mas';
+        btnVerMas2.className = 'btn btn-primary';
+    }
+
+    contenidoExpandido2 = !contenidoExpandido2;
+}
+ //card 4
+ let contenidoExpandido3 = false;
+
+function verMas3() {
+    const seccionServicio3 = document.getElementById('seccionServicio3');
+    const btnVerMas3 = document.getElementById('btnVerMas3');
+    const parrafoServicio3 = document.getElementById('parrafoServicio3');
+
+    if (!contenidoExpandido3) {
+        // Mostrar más contenido
+        parrafoServicio3.innerHTML += ` <br> <p class="text-justify p-2"> Ofrecemos servicios integrales de construcción para proyectos civiles, desde la planificación hasta la entrega final. Nos especializamos en estructuras sólidas, utilizando tecnologías avanzadas y materiales de alta calidad. Contamos con un experimentado equipo comprometido con la excelencia y altos estándares de calidad y seguridad. Además de la construcción, brindamos servicios de gestión de proyectos eficientes. Colaboramos estrechamente con nuestros clientes para crear espacios funcionales que reflejen su visión y estilo. Al elegirnos, obtienes confiabilidad y compromiso para convertir tus ideas en soluciones de construcción sólidas y sostenibles. </p>`;
+        btnVerMas3.innerHTML = 'Ocultar';
+        btnVerMas3.className = 'btn btn-danger';
+    } else {
+        // Restaurar el contenido original
+        parrafoServicio3.innerHTML = 'Servicio de sistemas de Extinción de incendios';
+        btnVerMas3.innerHTML = 'Ver mas';
+        btnVerMas3.className = 'btn btn-primary';
+    }
+
+    contenidoExpandido3 = !contenidoExpandido3;
+}
+//card 5
+let contenidoExpandido4 = false;
+
+function verMas4() {
+    const seccionServicio4 = document.getElementById('seccionServicio4');
+    const btnVerMas4 = document.getElementById('btnVerMas4');
+    const parrafoServicio4 = document.getElementById('parrafoServicio4');
+
+    if (!contenidoExpandido4) {
+        // Mostrar más contenido
+        parrafoServicio4.innerHTML += ` <br> <p class="text-justify p-2"> Ofrecemos servicios integrales de construcción para proyectos civiles, desde la planificación hasta la entrega final. Nos especializamos en estructuras sólidas, utilizando tecnologías avanzadas y materiales de alta calidad. Contamos con un experimentado equipo comprometido con la excelencia y altos estándares de calidad y seguridad. Además de la construcción, brindamos servicios de gestión de proyectos eficientes. Colaboramos estrechamente con nuestros clientes para crear espacios funcionales que reflejen su visión y estilo. Al elegirnos, obtienes confiabilidad y compromiso para convertir tus ideas en soluciones de construcción sólidas y sostenibles. </p>`;
+        btnVerMas4.innerHTML = 'Ocultar';
+        btnVerMas4.className = 'btn btn-danger';
+    } else {
+        // Restaurar el contenido original
+        parrafoServicio4.innerHTML = 'Servicio de alquiler de vehiculos y maquinarias.';
+        btnVerMas4.innerHTML = 'Ver mas';
+        btnVerMas4.className = 'btn btn-primary';
+    }
+
+    contenidoExpandido4 = !contenidoExpandido4;
+}
+//card 6
+let contenidoExpandido5 = false;
+
+function verMas5() {
+    const seccionServicio5 = document.getElementById('seccionServicio5');
+    const btnVerMas5 = document.getElementById('btnVerMas5');
+    const parrafoServicio5 = document.getElementById('parrafoServicio5');
+
+    if (!contenidoExpandido5) {
+        // Mostrar más contenido
+        parrafoServicio5.innerHTML += ` <br> <p class="text-justify p-2"> Ofrecemos servicios integrales de construcción para proyectos civiles, desde la planificación hasta la entrega final. Nos especializamos en estructuras sólidas, utilizando tecnologías avanzadas y materiales de alta calidad. Contamos con un experimentado equipo comprometido con la excelencia y altos estándares de calidad y seguridad. Además de la construcción, brindamos servicios de gestión de proyectos eficientes. Colaboramos estrechamente con nuestros clientes para crear espacios funcionales que reflejen su visión y estilo. Al elegirnos, obtienes confiabilidad y compromiso para convertir tus ideas en soluciones de construcción sólidas y sostenibles. </p>`;
+        btnVerMas5.innerHTML = 'Ocultar';
+        btnVerMas5.className = 'btn btn-danger';
+    } else {
+        // Restaurar el contenido original
+        parrafoServicio5.innerHTML = 'Servicio de alquiler de vehiculos y maquinarias.';
+        btnVerMas5.innerHTML = 'Ver mas';
+        btnVerMas5.className = 'btn btn-primary';
+    }
+
+    contenidoExpandido5 = !contenidoExpandido5;
+}
+
+
+
+
+
+
+
+
+// email js
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_a2s7ggc';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Correo Enviado';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Correo Enviado';
+      alert(JSON.stringify(err));
+    });
+});
